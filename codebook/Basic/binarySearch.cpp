343569904395cd5136e680d13f3d02865be191fe
DanielDu <danghoangnhan.1@gmail.com>
2021-07-01 08:24:05 +0800

    add 0630

--- codebook/Basic/binarySearch.cpp
+++ codebook/Basic/binarySearch.cpp
@@ -1,6 +1,8 @@
+#include <Basic/Template.h>
+
 lower_bound(a, a + n, k);     //最左邊 ≥ k 的位置
 upper_bound(a, a + n, k);     //最左邊 > k 的位置
 upper_bound(a, a + n, k) - 1; //最右邊 ≤ k 的位置
 lower_bound(a, a + n, k) - 1; //最右邊 < k 的位置
-[lower_bound, upper_bound) //等於 k 的範圍
+[lower_bound, upper_bound); //等於 k 的範圍
 equal_range(a, a+n, k);
--- codebook/DP/LCS.cpp
+++ codebook/DP/LCS.cpp
@@ -8,16 +8,13 @@ int main()
     cin >> n >> m;
     a.resize(n);
     b.resize(m);
-    for (int i = 0; i < a.size(); i++)
-    {
-        cin >> a[i];
-    }
-    for (int i = 0; i < b.size(); i++)
-    {
-        cin >> b[i];
-    }
+
+    for (int i = 0; i < a.size(); i++){cin >> a[i];}
+    for (int i = 0; i < b.size(); i++){cin >> b[i];}
+
     dp[0].resize(m + 1);
     dp[1].resize(m + 1);
+    
     for (int i = 1; i <= n; i++)
     {
         for (int j = 1; j <= m; j++)
